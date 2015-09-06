7502bf290eb9c9b931e3ae2ab39c08cdcff48d25
RLOC <rloc@itu.dk>
2015-09-06 16:40:01 +0200

    Parser_v0 added

--- IntellijProject/src/Parser.java
+++ IntellijProject/src/Parser.java
@@ -12,6 +12,9 @@ public class Parser {
         // i=2 due to the fact that input file always contains "n line" + "empty line"
         for (int i=2; i < fileLines.length; i++) {
 
+            String[] times = fileLines[i].split("\\s+");
+            jobs[i-2] = new Job(Integer.parseInt(times[0]), Integer.parseInt(times[1]));
+
         }
 
         return jobs;
