20aa730eb913a4ad90271a3e6699caba17e38884
Nemeczek <nemeczekes@gmail.com>
2015-09-07 10:37:00 +0200

    added class model for output

---
+++ IntellijProject/src/com/groupC/src/Output.java
@@ -0,0 +1,14 @@
+package com.groupC.src;
+
+/**
+ * Created by radush on 9/7/15.
+ */
+public class Output {
+    Job[] results;
+    int iterations;
+
+    public Output(Job[] results, int iterations) {
+        this.results = results;
+        this.iterations = iterations;
+    }
+}
