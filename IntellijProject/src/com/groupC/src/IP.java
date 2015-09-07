319d4ea6de83573ab38dd41814521a546b4a3617
Nemeczek <nemeczekes@gmail.com>
2015-09-07 11:35:02 +0200

    added empty Calculate method for running the alghoritm

--- IntellijProject/src/com/groupC/src/IP.java
+++ IntellijProject/src/com/groupC/src/IP.java
@@ -2,24 +2,27 @@ package com.groupC.src;
 import java.io.IOException;
 
 public class IP {
- public static void main(String[] args) throws IOException {
-     if (args.length > 0) {
-        FileReader fileReader = new FileReader();
-         // Parsing of fileinput - args[0]
-         String[] input = fileReader.ReadFile(args[0]);
+    public static void main(String[] args) throws IOException {
+         if (args.length > 0) {
+            FileReader fileReader = new FileReader();
+             // Parsing of fileinput - args[0]
+             String[] input = fileReader.ReadFile(args[0]);
 
 
-        // GaleShapley gs = Transformer.Transformation(input);
+            // GaleShapley gs = Transformer.Transformation(input);
 
-         //gs.StableMatcher();
+             //gs.StableMatcher();
 
-         System.out.println("\n");
-         System.out.println("Interval partitioning complete!");
-     }
-     else {
-         System.out.println("The program needs a filepath as input \n");
-         System.out.println("Terminating ...");
-     }
-
+             System.out.println("\n");
+             System.out.println("Interval partitioning complete!");
+         }
+         else {
+             System.out.println("The program needs a filepath as input \n");
+             System.out.println("Terminating ...");
+         }
  }
+
+    public Output Calculate(){
+        return null;
+    };
 }
