1811888be6742993ef85b3b9f343a67a71a5c10a
Nemeczek <nemeczekes@gmail.com>
2015-09-07 11:37:58 +0200

    added Junit4 to the project and test fixture for IP class

---
+++ IntellijProject/src/com/groupC/test/IPTest.java
@@ -0,0 +1,22 @@
+package com.groupC.test;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by radush on 9/7/15.
+ */
+public class IPTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
