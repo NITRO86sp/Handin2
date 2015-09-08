b9476081b484e01d4dc1e9058b40e2ff13667b6d
Nemeczek <nemeczekes@gmail.com>
2015-09-08 10:43:44 +0200

    added reading resource for output parser

--- IntellijProject/src/com/groupC/src/Parser.java
+++ IntellijProject/src/com/groupC/src/Parser.java
@@ -12,10 +12,8 @@ public class Parser {
         // creating the array of Job's
         // i=2 due to the fact that input file always contains "n line" + "empty line"
         for (int i=2; i < fileLines.length; i++) {
-
             String[] times = fileLines[i].split("\\s+");
             jobs[i-2] = new Job(Integer.parseInt(times[0]), Integer.parseInt(times[1]), i-2);
-
         }
 
         return jobs;
@@ -32,12 +30,10 @@ public class Parser {
         // creating the array of Job's
         // i=2 due to the fact that input file always contains "n line" + "empty line"
         for (int i=2; i < fileLines.length; i++) {
-
             String[] times = fileLines[i].split("\\s+");
             jobs[i-2] = new Job(Integer.parseInt(times[0]), Integer.parseInt(times[1]), i-2);
-
+            jobs[i-2].resource = Integer.parseInt(times[2]);
         }
-
         return jobs;
     }
 }
